271b60855a9d930c3ee1afa827e4bee36ec00c59
Rawan Azim <36305992+rawanazim@users.noreply.github.com>
2020-06-26 18:29:11 +0300

    Add files via upload

---
+++ face_detect.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+# choose the right path of the classifier file
+faceCascade = cv.CascadeClassifier('/home/rawan/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+
+cap = cv.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+cap.set(10,100)
+
+
+while True:
+	success, img = cap.read()
+	faces = faceCascade.detectMultiScale(img,1.1,4)
+	for (x,y,w,h) in faces:
+		cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	cv.imshow("video",img)
+	if cv.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv.destroyAllWindows()
+
